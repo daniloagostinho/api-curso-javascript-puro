ee68de78d6ded90059cd95ec7d892131442fb33f
Danilo Agostinho <daniloagostinhosilva@gmail.com>
2023-04-27 16:08:24 -0300

    Update IncomesController.js

--- controllers/IncomesController.js
+++ controllers/IncomesController.js
@@ -115,40 +115,43 @@ module.exports = class IncomesController {
 
   static async getIncomeStatement(req, res) {
     try {
-      const incomes = await Incomes.find({});
-
+      const { user } = req.headers;
+  
+      const incomes = await Incomes.find({ "user.title": user });
+  
       const incomeStatement = incomes.reduce((acc, income) => {
         const dateEntry = new Date(income.user.month.listMonth.dateEntry);
         const month = dateEntry.toLocaleDateString('default', { month: 'long' });
         const year = dateEntry.getFullYear();
-
+  
         const monthYear = `${month} ${year}`;
-
+  
         if (!acc[monthYear]) {
           acc[monthYear] = {
             month: monthYear,
             total: 0,
-            details: [],
+            details: []
           };
         }
-
+  
         const value = parseFloat(income.user.month.listMonth.value);
-
+  
         acc[monthYear].total += value;
         acc[monthYear].details.push({
           income: income.user.month.listMonth.income,
           value,
           dateEntry
         });
-
+  
         return acc;
       }, {});
-
+  
       const result = Object.values(incomeStatement);
-
+  
       res.status(200).json({ result });
     } catch (error) {
       res.status(500).json({ message: "Erro ao obter o extrato de receitas!" });
     }
   }
+  
 };
